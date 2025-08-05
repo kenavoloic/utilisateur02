c64e46788690c2fdc41aafe808bb273db5c692c6
kenavoloic <kenavoloic@gmail.com>
2025-08-06 01:40:48 +0200

    001

--- configurations/admin.py
+++ configurations/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from configurations.models import  Conducteur, Service, Site
+from configurations.models import  Conducteur, Service, Site, GroupePage, Page
 
 admin.site.register(Conducteur)
 admin.site.register(Service)
 admin.site.register(Site)
+admin.site.register(GroupePage)
+admin.site.register(Page)
 # Register your models here.
